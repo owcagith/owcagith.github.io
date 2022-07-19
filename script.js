f6283d4eab9af9c9aa03ea12891c69d10243ef27
OwCaGiT <107436635+owcagith@users.noreply.github.com>
2022-07-19 20:58:57 +0200

    new

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 /* Clock script */
 
-    function zegar() {
+    function clock() {
         var data = new Date();
         var godzina = data.getHours();
         var min = data.getMinutes();
         var sek = data.getSeconds();
-        var obecnie = ""+godzina+
-        ((min<10)?":0":":")+min+
-        ((sek<10)?":0":":")+sek;
+        var obecnie = godzina+ ":" + min + ":" + sek;
         document.getElementById("clock").innerHTML = obecnie;
-        setTimeout("zegar()", 1000); }
-        zegar();
+        setTimeout("clock()", 1000); }
+    clock();
 
 /* Aos script link */
 
